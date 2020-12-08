4333120a62c56047fcaf2048bedabbc16cf0ac54
jaq-cat <jaq.cat@protonmail.ch>
2020-12-08 16:35:38 -0300

    filter_map

--- src/iter.c
+++ src/iter.c
@@ -10,8 +10,19 @@ void map(int* arr, int len, int (*fun)(int, int)) {
 int filter(int* arr, int len, int (*fun)(int, int)) {
     int i=0;
     for (int j=0; j<len; j++)
-        if (CALL(fun, i, arr[i])) {
+        if (CALL(fun, j, arr[j])) {
             arr[i] = arr[j];
+            i++;
+        }
+    return i+1; // len
+}
+
+int filter_map(int* arr, int len, int (*fun)(int, int)) {
+    int i=0;
+    int r;
+    for (int j=0; j<len; j++)
+        if ((r = CALL(fun, j, arr[j]))) {
+            arr[i] = r;
             i++;
         }
     return i+1; // len
